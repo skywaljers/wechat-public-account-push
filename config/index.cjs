dc075450ab076b11e85a34d10c0c46459c3b38bc
skywaljers <86068838+skywaljers@users.noreply.github.com>
2023-07-13 14:27:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,60 +7,52 @@
  */
 const USER_CONFIG = {
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
-  USERS: [
+ "APP_ID":" wx0429c65b787d4cc9",
+    "APP_SECRET":"0733de7d43320d85e9a82f534419c6d2",
+    "IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"2",
+    "CALLBACK_USERS":[{"name":"自己","id":"1"}],
+    "USERS":
+    [
+        {
+            "name":"小聪明蛋",
+            "id":"2",
+            "useTemplateId":"1",
+            "province":"贵州",
+            "city":"六盘水",
+            "horoscopeDate":"05-30",
+            "horoscopeDateType":"今日",
+            "openUrl":"1",
+            "festivals":
+            [
+                {
+                    "type":"节日",
+                    "name":"认识的第一天",
+                    "date":"08-02",
+                    "year":"2022"
+                }
+            ],
+                "customizedDateList":
+                [
+                    {
+                        "keyword":"认识的第一天",
+                        "date":"2023-08-02"
+                    }
+                ]
+        }
+    ],
+    "SWITCH":
     {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+        "weather":true,
+        "holidaytts":true,
+        "CIBA":true,
+        "oneTalk":false,
+        "earthyLoveWords":false,
+        "momentCopyrighting":false,
+        "poisonChickenSoup":false,
+        "poetry":false,
+        "horoscope":false,
+        "birthdayMessage":true
     }
-  ],
-
 }
 
 module.exports = USER_CONFIG
